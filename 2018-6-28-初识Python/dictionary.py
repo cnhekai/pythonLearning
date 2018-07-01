98e686c084adbe2072a02873d63348648b17cdc1
cnhekai <cnhekai@outlook.com>
2018-07-02 00:35:34 +0800

    字典学习

--- 2018-6-28-初识Python/dictionary.py
+++ 2018-6-28-初识Python/dictionary.py
@@ -9,3 +9,8 @@ print(dictionary[2]) # 输出键为2的值
 print(tinydict) # 输出tinydict所有的值
 print(tinydict.keys()) # 输出tinydict所有的键
 print(tinydict.values()) # 输出tinydict所有的值
+
+dictionary['addKey'] = 'addValue' # 添加一个key为addKey的键值对
+print(dictionary.get('addKey')) # 打印新添加的key为addKey的值
+dictionary.pop('addKey') # 删除key为addKey的键值对
+print(dictionary.get('addKey',-1)) # 如果key不存在，可以返回None，或者自己指定的value
